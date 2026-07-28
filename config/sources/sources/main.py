467266b7602ab3d750b3d4149d2ba8e1c73ba234
Duverie <m-j@gmx.net>
2026-07-28 13:18:12 +0200

    Create main.py

---
+++ config/sources/sources/main.py
@@ -0,0 +1,13 @@
+from data.reader import zeige_hoefe
+
+
+def starten():
+    print("=" * 50)
+    print("🏡 HofRadar gestartet")
+    print("=" * 50)
+
+    zeige_hoefe()
+
+
+if __name__ == "__main__":
+    starten()
