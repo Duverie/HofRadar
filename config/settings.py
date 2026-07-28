f14c1f8ab87613a45b891240ae654cc42c602154
Duverie <m-j@gmx.net>
2026-07-28 13:06:43 +0200

    data/.gitkeep

---
+++ config/settings.py
@@ -0,0 +1,18 @@
+# HofRadar Einstellungen
+
+PROJECT_NAME = "HofRadar"
+
+MAX_PREIS = 1500000
+MIN_GRUNDSTUECK = 4000
+
+REGIONEN = [
+    "Schweiz",
+    "Elsass",
+    "Südbaden"
+]
+
+TIERE = [
+    "2 Pferde",
+    "Hund",
+    "Katzen"
+]
